6780d46573ec85d215d93d1c972f70e194ecddb6
monmonplaza <monmon.plaza@gmail.com>
2024-11-04 13:35:48 +0800

    123

--- src/components/pages/partials/ToastSuccess.jsx
+++ src/components/pages/partials/ToastSuccess.jsx
@@ -1,21 +1,33 @@
-import { CheckCircle, X } from 'lucide-react'
-import React from 'react'
+import { setSuccess } from "@/components/store/StoreAction.jsx";
+import { StoreContext } from "@/components/store/StoreContext.jsx";
+import { CheckCircle, X } from "lucide-react";
+import React from "react";
 
 const ToastSuccess = () => {
+  const { store, dispatch } = React.useContext(StoreContext);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      dispatch(setSuccess(false));
+    }, 3000);
+  }, []);
   return (
-    <div className='fixed top-5 left-1/2 -translate-x-1/2  bg-secondary max-w-[240px] w-full overflow-hidden rounded-md shadow-md border-success border'>
-        <div className="flex items-center r">
-            <div className='grid place-content-center bg-success basis-[34px] size-[34px]'>
-                <CheckCircle size={18} className='text-white'/>
-            </div>
-
-            <p className='basis-[80%] p-2 mb-0'>Record Successfully Updated!</p>
-
-            <button className='basis-[10%]'><X size={14}/></button>
+    <div className="fixed top-5 left-1/2 -translate-x-1/2  bg-secondary max-w-[300px] w-full overflow-hidden rounded-md shadow-md border-success border">
+      <div className="flex items-center r">
+        <div className="grid place-content-center bg-success basis-[34px] size-[34px]">
+          <CheckCircle size={18} className="text-white" />
         </div>
 
-    </div>
-  )
-}
+        <p className="basis-[80%] p-2 mb-0 text-xs">
+          Record Successfully Updated!
+        </p>
 
-export default ToastSuccess
+        <button className="basis-[10%]">
+          <X size={14} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ToastSuccess;
--- src/components/pages/ramen/Ramen.jsx
+++ src/components/pages/ramen/Ramen.jsx
@@ -1,21 +1,27 @@
+import { StoreContext } from "@/components/store/StoreContext.jsx";
 import React from "react";
-import Navigation from "../partials/Navigation";
-import Header from "../partials/Header";
 import Footer from "../partials/Footer";
+import Header from "../partials/Header";
+import Navigation from "../partials/Navigation";
+import ToastSuccess from "../partials/ToastSuccess.jsx";
 import RamenTable from "./RamenTable";
 
 const Ramen = () => {
+  const { store, dispatch } = React.useContext(StoreContext);
   return (
-    <section className="flex  min-h-screen bg-secondary">
-      <aside className="bg-primary text-dark basis-[200px] border-r border-gray-200">
-        <Navigation menu="movies" />
-      </aside>
-      <main className="basis-[calc(100%-200px)] min-h-[100vh] grid grid-rows-[auto_1fr_auto]">
-        <Header title="Ramen" subtitle="List of Movies" />
-        <RamenTable />
-        <Footer />
-      </main>
-    </section>
+    <>
+      <section className="flex  min-h-screen bg-secondary">
+        <aside className="bg-primary text-dark basis-[200px] border-r border-gray-200">
+          <Navigation menu="movies" />
+        </aside>
+        <main className="basis-[calc(100%-200px)] min-h-[100vh] grid grid-rows-[auto_1fr_auto]">
+          <Header title="Ramen" subtitle="List of Movies" />
+          <RamenTable />
+          <Footer />
+        </main>
+      </section>
+      {store.success && <ToastSuccess />}
+    </>
   );
 };
 
